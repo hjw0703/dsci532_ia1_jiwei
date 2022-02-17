cfe9b9eaf3132679595c7db1c1a3e92a6810fa4f
hjw0703 <hjw0703@student.ubc.ca>
2022-02-17 09:27:07 -0800

    second ccommit

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ import pandas as pd
 
 
 # Read in global data
-df = pd.read_csv('~/mds/block5/DSCI_532_IA1/cost_of_living_processed.csv')
-df = df[df['country'] == 'Canada']
+df = pd.DataFrame({
+'city' : ['Calgary','Edmonton','Ottawa','Toronto','Vancouver','Victoria','Winnipeg','Montreal']
+'rent_for_one_person' : [852.13, 867.61, 973.04, 1513.37, 1458.08, 1094.24, 752.15, 811.25],
+'transportation_public': [122.4, 114.66, 132.68, 161.55, 121.26, 112.91, 118.73, 108.84],
+'grocery_for_one_person': [264.62, 252.76, 255.04, 320.84, 309.81, 300.18, 239.9, 294.17],
+'entertainment': [268.54, 253.03, 246.92, 274.79, 272.69, 271.41, 242.0, 239.99],
+'fitness': [119.49, 104.79, 122.63, 144.16, 89.38, 75.52, 104.8, 77.2],
+'utility_bills': [219.95, 243.96, 173.29, 168.38, 135.84, 158.71, 161.73, 132.37],
+'shopping': [255.77, 258.11, 287.04, 278.8, 288.83, 292.16, 239.43, 258.76],
+'monthly_salary': [3370.22, 2909.53, 3311.32, 2682.07, 2992.85, 2665.73, 2358.51, 2488.06],
+'monthly_saving': [1267.32, 814.6, 1120.68, -179.82, 316.97, 360.59, 499.76, 565.49]
+})
 
 # Setup app and layout/frontend
 app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
